46eed267287e217456e688dcda949e4cb1fa7ee8
Phorcys <gpg@phorcys.net>
2022-11-11 00:14:59 +0100

    Use same handler for both /resolve and /unresolve

--- lib/discord/commands/resolve.dart
+++ lib/discord/commands/resolve.dart
@@ -1,9 +1,82 @@
 import "package:codercord/config.dart";
 import "package:codercord/discord/utils.dart";
+
 import "package:nyxx/nyxx.dart";
 import "package:nyxx_interactions/nyxx_interactions.dart";
 
-// TODO: Use generalized handler for "resolve" and "unresolve"
+Future<void> handleResolve(
+    ISlashCommandInteractionEvent p0, bool resolve) async {
+  await p0.acknowledge();
+
+  final resolvedWord = resolve == true ? "resolved" : "unresolved";
+
+  final threadChannel =
+      await p0.interaction.channel.download() as IThreadChannel;
+
+  if (threadChannel.channelType == ChannelType.guildPublicThread) {
+    if (canUserInteractWithThread(threadChannel.owner, p0.interaction)) {
+      if (config["helpChannel"]["id"] ==
+              threadChannel.parentChannel?.id.id.toString() &&
+          await threadChannel.isForumPost) {
+        final postTags = await threadChannel.appliedTags;
+
+        try {
+          if (resolve &&
+              !postTags.contains(config["helpChannel"]["resolvedTag"])) {
+            await threadChannel.setPostTags(
+              postTags..add(config["helpChannel"]["resolvedTag"]),
+            );
+          } else if (!resolve &&
+              postTags.contains(config["helpChannel"]["resolvedTag"])) {
+            await threadChannel.setPostTags(
+              postTags
+                ..removeWhere(
+                  (e) => e == config["helpChannel"]["resolvedTag"],
+                ),
+            );
+          } else {
+            await p0.respond(
+              MessageBuilder.content("Thread is already $resolvedWord."),
+              hidden: true,
+            );
+          }
+
+          await p0.respond(
+            MessageBuilder.content("Marked the thread as $resolvedWord."),
+            hidden: true,
+          );
+        } catch (e) {
+          await p0.respond(
+            MessageBuilder.content(
+                "Could not mark the thread as $resolvedWord because of an unexpected error."),
+            hidden: true,
+          );
+        }
+      }
+
+      if (threadChannel.archived == !resolve) {
+        try {
+          await threadChannel.archive(resolve);
+        } catch (_) {}
+      }
+    } else {
+      p0.respond(
+        MessageBuilder.content(
+          "You cannot mark this thread as $resolvedWord since you are not the OP.",
+        ),
+        hidden: true,
+      );
+    }
+  } else {
+    p0.respond(
+      MessageBuilder.content(
+        "You can only run this command in a thread/forum post.",
+      ),
+      hidden: true,
+    );
+  }
+}
+
 SlashCommandBuilder getCommand() {
   return SlashCommandBuilder(
     "resolve",
@@ -22,65 +95,6 @@ SlashCommandBuilder getCommand() {
     guild: Snowflake(config["coderServer"]["id"]),
     canBeUsedInDm: false,
   )..registerHandler((p0) async {
-      await p0.acknowledge();
-
-      final threadChannel =
-          await p0.interaction.channel.download() as IThreadChannel;
-
-      if (threadChannel.channelType == ChannelType.guildPublicThread) {
-        if (canUserInteractWithThread(threadChannel.owner, p0.interaction)) {
-          // only attempt to add the tag if in the help forum channel
-          if (config["helpChannel"]["id"] ==
-                  threadChannel.parentChannel?.id.id.toString() &&
-              await threadChannel.isForumPost) {
-            final postTags =
-                await threadChannel.appliedTags; // async getters, I know
-
-            if (!postTags.contains(config["helpChannel"]["resolvedTag"])) {
-              try {
-                await threadChannel.setPostTags(
-                  postTags..add(config["helpChannel"]["resolvedTag"]),
-                );
-
-                await p0.respond(
-                  MessageBuilder.content("Marked the thread as resolved."),
-                  hidden: true,
-                );
-              } catch (e) {
-                await p0.respond(
-                  MessageBuilder.content(
-                      "Could not mark the thread as resolved because of an unexpected error."),
-                  hidden: true,
-                );
-              }
-            } else {
-              await p0.respond(
-                MessageBuilder.content("Thread is already resolved."),
-                hidden: true,
-              );
-            }
-          }
-
-          if (!threadChannel.archived) {
-            try {
-              await threadChannel.archive(true, p0.args[0].value);
-            } catch (_) {}
-          }
-        } else {
-          p0.respond(
-            MessageBuilder.content(
-              "You cannot resolve the thread since you are not the OP.",
-            ),
-            hidden: true,
-          );
-        }
-      } else {
-        p0.respond(
-          MessageBuilder.content(
-            "You can only run this command in a thread/forum post.",
-          ),
-          hidden: true,
-        );
-      }
+      await handleResolve(p0, true);
     });
 }
--- lib/discord/commands/unresolve.dart
+++ lib/discord/commands/unresolve.dart
@@ -1,5 +1,6 @@
 import "package:codercord/config.dart";
-import "package:codercord/discord/utils.dart";
+import "package:codercord/discord/commands/resolve.dart" show handleResolve;
+
 import "package:nyxx/nyxx.dart";
 import "package:nyxx_interactions/nyxx_interactions.dart";
 
@@ -11,66 +12,6 @@ SlashCommandBuilder getCommand() {
     guild: Snowflake(config["coderServer"]["id"]),
     canBeUsedInDm: false,
   )..registerHandler((p0) async {
-      await p0.acknowledge();
-
-      final threadChannel =
-          await p0.interaction.channel.download() as IThreadChannel;
-
-      if (threadChannel.channelType == ChannelType.guildPublicThread) {
-        if (canUserInteractWithThread(threadChannel.owner, p0.interaction)) {
-          if (config["helpChannel"]["id"] ==
-                  threadChannel.parentChannel?.id.id.toString() &&
-              await threadChannel.isForumPost) {
-            final postTags = await threadChannel.appliedTags;
-
-            if (postTags.contains(config["helpChannel"]["resolvedTag"])) {
-              try {
-                await threadChannel.setPostTags(
-                  postTags
-                    ..removeWhere(
-                      (e) => e == config["helpChannel"]["resolvedTag"],
-                    ),
-                );
-
-                await p0.respond(
-                  MessageBuilder.content("Marked the thread as unresolved."),
-                  hidden: true,
-                );
-              } catch (e) {
-                await p0.respond(
-                  MessageBuilder.content(
-                      "Could not mark the thread as unresolved because of an unexpected error."),
-                  hidden: true,
-                );
-              }
-            } else {
-              await p0.respond(
-                MessageBuilder.content("Thread is already unresolved."),
-                hidden: true,
-              );
-            }
-          }
-
-          if (!threadChannel.archived) {
-            try {
-              await threadChannel.archive(false);
-            } catch (_) {}
-          }
-        } else {
-          p0.respond(
-            MessageBuilder.content(
-              "You cannot mark this thread as unresolved since you are not the OP.",
-            ),
-            hidden: true,
-          );
-        }
-      } else {
-        p0.respond(
-          MessageBuilder.content(
-            "You can only run this command in a thread/forum post.",
-          ),
-          hidden: true,
-        );
-      }
+      await handleResolve(p0, false);
     });
 }
