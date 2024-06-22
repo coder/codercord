fef6f051ee601c78749ccb617e18f64af7016535
phorcys420 <57866459+phorcys420@users.noreply.github.com>
2024-06-22 21:40:02 +0000

    feat: push base projet with linting/formatting/etc and my current idea of the code structure

---
+++ src/lib/config.ts
@@ -0,0 +1,17 @@
+import loadConfig from "@uwu/configmasher";
+
+interface Config {
+    token: string;
+}
+
+export const { config, layers } = await loadConfig<Config>({
+    name: "Codercord",
+
+    environmentFile: true,
+    processEnvironment: true,
+
+    caseInsensitive: true,
+
+    defaults: {},
+    mandatory: ["token"],
+});
