d660a604e3e5bcbea0d82e32ff269d311da1046a
Muhammad Atif Ali <me@matifali.dev>
2024-06-22 23:44:52 +0000

    use 2 spaces for indent

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,30 +2,30 @@ import fs from "node:fs/promises";
 import path from "node:path";
 
 export async function loadCommands(): Promise<Map<string, unknown>> {
-	const commands = new Map();
+  const commands = new Map();
 
-	const foldersPath = path.join(__dirname, "commands");
-	const commandFolders = await fs.readdir(foldersPath);
+  const foldersPath = path.join(__dirname, "commands");
+  const commandFolders = await fs.readdir(foldersPath);
 
-	for (const folder of commandFolders) {
-		const commandsPath = path.join(foldersPath, folder);
-		const commandFiles = await fs
-			.readdir(commandsPath)
-			.then((files) => files.filter((file) => file.endsWith(".js")));
+  for (const folder of commandFolders) {
+    const commandsPath = path.join(foldersPath, folder);
+    const commandFiles = await fs
+      .readdir(commandsPath)
+      .then((files) => files.filter((file) => file.endsWith(".js")));
 
-		for (const file of commandFiles) {
-			const filePath = path.join(commandsPath, file);
-			const command = await import(filePath);
+    for (const file of commandFiles) {
+      const filePath = path.join(commandsPath, file);
+      const command = await import(filePath);
 
-			if ("data" in command && "execute" in command) {
-				commands.set(command.data.name, command);
-			} else {
-				throw new Error(
-					`The command at ${filePath} is missing a required "data" or "execute" property.`,
-				);
-			}
-		}
-	}
+      if ("data" in command && "execute" in command) {
+        commands.set(command.data.name, command);
+      } else {
+        throw new Error(
+          `The command at ${filePath} is missing a required "data" or "execute" property.`,
+        );
+      }
+    }
+  }
 
-	return commands;
+  return commands;
 }
