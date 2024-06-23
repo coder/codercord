ab32fac4006b59a2463e2eaefb3a44cd309307bf
Muhammad Atif Ali <me@matifali.dev>
2024-06-23 00:39:31 +0000

    chnage indent to 2 spaces

--- src/commands/util/walkthrough.ts
+++ src/commands/util/walkthrough.ts
@@ -1,11 +1,11 @@
 import { SlashCommandBuilder } from "discord.js";
 
 export default {
-    data: new SlashCommandBuilder()
-        .setName("ping")
-        .setDescription("Replies with Pong!"),
+  data: new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with Pong!"),
 
-    async execute(interaction) {
-        await interaction.reply("Pong!");
-    },
+  async execute(interaction) {
+    await interaction.reply("Pong!");
+  },
 };
